18630ab1375d0fb689533918a5798ec64345887c
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-02-10 17:11:10 +0100

    LinkedList

---
+++ src/main/java/com/piotrgrochowiecki/Main.java
@@ -0,0 +1,27 @@
+package com.piotrgrochowiecki;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<String> shoppingList = new LinkedList<>();
+        shoppingList.add("apple");
+        shoppingList.add("banana");
+        shoppingList.add("pear");
+
+        System.out.println(shoppingList);
+
+        shoppingList.add(1, "mango");
+        System.out.println(shoppingList);
+
+        shoppingList.removeFirst();
+        System.out.println(shoppingList);
+
+        List<String> synchronizedShoppingList = Collections.synchronizedList(shoppingList);
+        System.out.println(synchronizedShoppingList);
+    }
+
+}
